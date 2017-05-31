84cfb9ef44009bc12804c56aaead129e17bd6e2b
Nate Gay <nate.gay@wpengine.com>
2017-05-31 03:22:57 -0500

    International Law

--- functions.php
+++ functions.php
@@ -52,3 +52,57 @@ function create_custom_taxonomy() {
   );
 }
 add_action( 'init', 'create_custom_taxonomy' );
+
+/*
+ * Check if users can register and if the default role is set to student
+ */
+
+function check_if_user_registration_enabled() {
+  $registration = $wpdb->get_var( 'SELECT option_value FROM $wpdb->options WHERE option_name = "users_can_register"' );
+  if ($registration == 1) {
+    return true;
+  }
+  return false;
+}
+function set_user_registration_enabled() {
+  $response = $wpdb->query('UPDATE $wpdb->options SET option_value = 1 WHERE option_name = "users_can_register"');
+  return $response;
+}
+
+function check_if_default_role_is_student() {
+  $role = $wpdb->get_var( 'SELECT option_value FROM $wpdb->options WHERE option_name = "default_role"' );
+  if ($role == "figgins_student") {
+    return true;
+  }
+  return false;
+}
+function set_default_role_to_student() {
+  $response = $wpdb->query('UPDATE $wpdb->options SET option_value = "figgins_student" WHERE option_name = "default_role"');
+  return $response;
+}
+
+/*
+ * Redirect students when logging in
+ */
+
+function student_login_redirect( $redirect_to, $request, $user ) {
+  if ( isset( $user->roles ) && is_array( $user->roles ) ) {
+    if ( in_array( 'figgins_student', $user->roles ) ) {
+    	return '/student';
+    }
+    return $redirect_to;
+  }
+  return $redirect_to;
+}
+add_filter( 'login_redirect', 'student_login_redirect', 10, 3 );
+
+/*
+ * Disable admin bar if not administrator
+ */
+
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
+add_action('after_setup_theme', 'remove_admin_bar');
